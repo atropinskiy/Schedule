52886ed1b84359853824ffc9cf13d0b020af3f65
atropinskiy <vaskaop@mail.ru>
2024-12-27 14:53:48 +0300

    Sprint_18 start

---
+++ Schedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Schedule
+//
+//  Created by alex_tr on 27.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
